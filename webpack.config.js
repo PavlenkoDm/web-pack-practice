92d8bcf2db68c7ea2bbae31c131ec60fc858bea2
PavlenkoDm <dmpavlenko07@gmail.com>
2023-02-19 00:15:53 +0300

    add webpack basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,22 +10,31 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'project-bundle.js',
-    },
-    plugins: [new HtmlWebpackPlugin()],
-    module: {//--Loaders---
+    },    
+    module: {//--Loaders-------------------------------------------------------------------------
         rules: [
             {
                 test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"]
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,/*Извлекатель CSS кода в отдельный файл*/
+                    'css-loader',
+                    'sass-loader'
+                ]
             },
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"]
+                use: ['babel-loader']
             }   
         ]
     },
-    devServer: {//--Server---
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new MiniCssExtractPlugin({ filename: 'styles.css' }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {//--Server------------------------------------------------------------------------
         open: true,
         port: 7777,
         stats: 'errors-only'
